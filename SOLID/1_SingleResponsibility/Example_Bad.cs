815afda0390cbd939bd5bb5b22cd0ade59e274ae
derek-baker <dbaker@sdgnys.com>
2020-07-12 21:22:36 -0400

    SRP

---
+++ SOLID/1_SingleResponsibility/Example_Bad.cs
@@ -0,0 +1,36 @@
+﻿namespace SOLID._1_SingleResponsibility.Bad
+{
+    internal interface IReportData { }
+
+    internal interface IDb { internal IReportData GetData(); }
+
+    internal interface IReportingEngine { internal byte[] GenerateReport(IReportData data); }
+
+    internal interface IReportGenerator { }
+
+    /// <summary>
+    /// This class could require modification due to changes in  
+    /// how data is acquired, or by changes in how the report is generated.
+    /// </summary>
+    internal class ReportGenerator : IReportGenerator 
+    {
+        private readonly IDb _db;
+        private readonly IReportingEngine _reportEngine;        
+
+        internal ReportGenerator(IDb db, IReportingEngine reportEngine)
+        {
+            _db = db;
+            _reportEngine = reportEngine;
+        }
+
+        internal IReportData GetReportData()
+        {
+            return _db.GetData();
+        }
+
+        internal byte[] GenerateReport(IReportData data)
+        {
+            return _reportEngine.GenerateReport(data);
+        }
+    }
+}
--- SOLID/5_DependencyInversion/Example_Bad.cs
+++ SOLID/5_DependencyInversion/Example_Bad.cs
@@ -5,12 +5,12 @@
     internal class Controller
     {
         // We're coupling the class to a specific implementation of one of its dependencies.
-        private readonly Logger logger;
+        private readonly Logger _logger;
 
         internal Controller()
         {
             // Since we're allowing the dependency-creation here, we also need to know how to configure the dependency.
-            logger = new Logger();
+            _logger = new Logger();
             // If we need to use a different object, we'll need to modify this class, and risk introducing a regression.
         }
     }
